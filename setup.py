9543f76bc940fe22cb1a7bc973894ba859f4b507
Mitchell Stokes <mogurijin@gmail.com>
2013-12-17 23:35:55 -0800

    Modifying setup.py to grab Django.

--- setup.py
+++ setup.py
@@ -1,15 +1,5 @@
 from setuptools import setup
 
-setup(name='YourAppName', version='1.0',
-      description='OpenShift Python-3.3 Community Cartridge based application',
-      author='Your Name', author_email='ramr@example.org',
-      url='http://www.python.org/sigs/distutils-sig/',
-
-      #  Uncomment one or more lines below in the install_requires section
-      #  for the specific client drivers/modules your application needs.
-      install_requires=['CherryPy',
-                        #  'mysql-connector-python',
-                        #  'pymongo',
-                        #  'psycopg2',
-      ],
-     )
+setup(name='LegendGen',
+    install_requires=['django>=1.6'],
+)
