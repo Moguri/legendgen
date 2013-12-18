5ec568c60c715dcfb3b1f9ed611278b6cb41aabb
Mitchell Stokes <mogurijin@gmail.com>
2013-12-18 00:57:30 -0800

    Make sure os is imported before trying to use it...

--- legendgen/settings.py
+++ legendgen/settings.py
@@ -8,11 +8,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-IN_OPENSHIFT = 'OPENSHIFT_REPO_DIR' in os.eviron
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+IN_OPENSHIFT = 'OPENSHIFT_REPO_DIR' in os.eviron
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
